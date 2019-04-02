3d5edd751d5839c46c5ef907a47182d41c9ae322
lingqiz <zlqzcc@gmail.com>
2019-04-01 22:45:44 -0400

    add motor parameter

--- main.m
+++ main.m
@@ -1,49 +1,54 @@
 % Combined Subject
+paraInit  = [1, 150, 0.015];
+optOption = 'bads';
 %% Without Feedback
 load('woFB_td.mat');
-paraInit = [1, 50];
-[para_woFB_td, ~] = optWrapper(paraInit, allTarget, allResponse, 'bads');
+[para_woFB_td] = optWrapper(paraInit, allTarget, allResponse, optOption);
 
 load('woFB_asd.mat');
-paraInit = [1, 50];
-[para_woFB_asd, ~] = optWrapper(paraInit, allTarget, allResponse, 'bads');
+[para_woFB_asd] = optWrapper(paraInit, allTarget, allResponse, optOption);
 
 %% With Feedback1
 load('wFB1_td.mat');
-paraInit = [1, 50];
-[para_wFB1_td, ~] = optWrapper(paraInit, allTarget, allResponse, 'bads');
+[para_wFB1_td] = optWrapper(paraInit, allTarget, allResponse, optOption);
 
 load('wFB1_asd.mat');
-paraInit = [1, 50];
-[para_wFB1_asd, ~] = optWrapper(paraInit, allTarget, allResponse, 'bads');
+[para_wFB1_asd] = optWrapper(paraInit, allTarget, allResponse, optOption);
 
 %% With Feedback2
 load('wFB2_td.mat');
-paraInit = [1, 50];
-[para_wFB2_td, ~] = optWrapper(paraInit, allTarget, allResponse, 'bads');
+[para_wFB2_td] = optWrapper(paraInit, allTarget, allResponse, optOption);
 
 load('wFB2_asd.mat');
-paraInit = [1, 50];
-[para_wFB2_asd, ~] = optWrapper(paraInit, allTarget, allResponse, 'bads');
+[para_wFB2_asd] = optWrapper(paraInit, allTarget, allResponse, optOption);
 
 %% Plot Parameter Change
-figure; subplot(1, 2, 1);
+figure; subplot(3, 1, 1);
 hold on; grid on;
 plot([0.8, 2, 3.2], [para_woFB_td(1), para_wFB1_td(1), para_wFB2_td(1)], '--o', 'LineWidth', 2);
 plot([0.8, 2, 3.2], [para_woFB_asd(1), para_wFB1_asd(1), para_wFB2_asd(1)], '--o', 'LineWidth', 2);
 legend({'Normal', 'ASD'});
-xlim([0, 4]); xticks([0.8, 2, 3.2]);
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
 xticklabels({'woFB', 'wFB1', 'wFB2'});
 title('Prior Parameter');
 
-subplot(1, 2, 2);
+subplot(3, 1, 2);
 hold on; grid on;
 plot([0.8, 2, 3.2], [1/para_woFB_td(2), 1/para_wFB1_td(2), 1/para_wFB2_td(2)], '--o', 'LineWidth', 2);
 plot([0.8, 2, 3.2], [1/para_woFB_asd(2), 1/para_wFB1_asd(2), 1/para_wFB2_asd(2)], '--o', 'LineWidth', 2);
 legend({'Normal', 'ASD'});
-xlim([0, 4]); xticks([0.8, 2, 3.2]);
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
 xticklabels({'woFB', 'wFB1', 'wFB2'});
-title('Noise Parameter');
+title('Int Noise Parameter');
+
+subplot(3, 1, 3);
+hold on; grid on;
+plot([0.8, 2, 3.2], [para_woFB_td(3), para_wFB1_td(3), para_wFB2_td(3)], '--o', 'LineWidth', 2);
+plot([0.8, 2, 3.2], [para_woFB_asd(3), para_wFB1_asd(3), para_wFB2_asd(3)], '--o', 'LineWidth', 2);
+legend({'Normal', 'ASD'});
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
+xticklabels({'woFB', 'wFB1', 'wFB2'});
+title('Mtr Noise Parameter');
 
 %% Plot Model Prediction, Normal
 figure; subplot(1, 2, 1); hold on; grid on;
--- optWrapper.m
+++ optWrapper.m
@@ -5,16 +5,16 @@ responseRadius = allResponse/180 * pi;
 targetRadius(targetRadius < 0)   = targetRadius(targetRadius < 0) + 2 * pi;
 responseRadius(responseRadius < 0) = responseRadius(responseRadius < 0) + 2 * pi;
 
-lb = [0, 1];
-ub = [2, 200];
-objFunc = @(para)dataLlhd(para(1), para(2), targetRadius, responseRadius);
+lb = [0, 1, 1e-2];
+ub = [2, 500, 0.25];
+objFunc = @(para)dataLlhd(para(1), para(2), para(3), targetRadius, responseRadius);
 
 if (strcmp(optFunc, 'fminsearch'))
     opts = optimset('fminsearch');
     opts.Display = 'iter';
     opts.TolX = 1e-4;
-    opts.TolFun = 1e-4;    
-        
+    opts.TolFun = 1e-4;
+    
     [para, fval] = fminsearchbnd(objFunc, init, [0, 1], [2, 200], opts);
 elseif (strcmp(optFunc, 'fmincon'))
     opts = optimoptions('fmincon');
@@ -24,8 +24,8 @@ elseif (strcmp(optFunc, 'fmincon'))
     
     [para, fval] = fmincon(objFunc, init, [], [], [], [], lb, ub, [], opts);
 elseif (strcmp(optFunc, 'bads'))
-    plb = [0.1, 10];
-    pub = [2, 100];
+    plb = [0.1, 40, 0.01];
+    pub = [2, 400, 0.1];
     [para, fval] = bads(objFunc, init, lb, ub, plb, pub);
 else
     error('Invalid optimization option.');
